6d4ce4ae6a753ea5202f2743aaa54351e1eafc93
nguyenhuuca <nguyenhuuca@gmail.com>
2017-07-09 23:21:26 +0700

    adding Entity Manager

--- HealthCare/src/test/java/com/canh/healthcare/AppTest.java
+++ HealthCare/src/test/java/com/canh/healthcare/AppTest.java
@@ -1,10 +1,12 @@
 package com.canh.healthcare;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 
 import com.canh.healthcare.jpa.entity.AppUser;
+import com.canh.healthcare.jpa.utils.EntityManagerUtil;
 import com.canh.healthcare.jpa.utils.HibernateUtil;
 
 import junit.framework.Test;
@@ -14,46 +16,56 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	private EntityManager entityManager;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-    
-    public void testApp1() {
-		//SessionFactory sessionFactory = new Configuration().configure()
-		//		.buildSessionFactory();
-		//Session session = sessionFactory.openSession();
-    	Session session = HibernateUtil.getSessionFactory().openSession();
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+		assertTrue(true);
+	}
+
+	public void testApp1() {
+		// SessionFactory sessionFactory = new Configuration().configure()
+		// .buildSessionFactory();
+		// Session session = sessionFactory.openSession();
+		Session session = HibernateUtil.getSessionFactory().openSession();
 		session.beginTransaction();
- 
-		AppUser user = new AppUser("firstuser","123456","test");
+
+		AppUser user = new AppUser("firstuser", "123456", "test");
 		session.save(user);
- 
+
 		session.getTransaction().commit();
 		session.close();
+	}
+
+	public void testApp2() {
+		entityManager = EntityManagerUtil.getEntityManager();
+
+		entityManager.getTransaction().begin();
+
+		AppUser user = new AppUser("firstuser", "123456", "test_entity");
+		entityManager.persist(user);
+
+		entityManager.getTransaction().commit();
+		entityManager.close();
 	}
 }
