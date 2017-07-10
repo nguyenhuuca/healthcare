6397c1f66eba5b808be025ffe34454cabec91f23
nguyenhuuca <canh@evolableasia.vn>
2017-07-10 19:09:35 +0900

    add model

--- HealthCare/src/main/java/com/canh/healthcare/model/Patient.java
+++ HealthCare/src/main/java/com/canh/healthcare/model/Patient.java
@@ -1,8 +1,16 @@
 package com.canh.healthcare.model;
 
+import java.util.Date;
+
 public class Patient {
 	int id;
-	int name;
+	String name;
+	Date birthDay;
+	boolean male;
+	String address;
+	String mobile;
+	String familyContact;
+	Date firstJoin = new Date();
 
 	public int getId() {
 		return id;
@@ -12,12 +20,60 @@ public class Patient {
 		this.id = id;
 	}
 
-	public int getName() {
+	public String getName() {
 		return name;
 	}
 
-	public void setName(int name) {
+	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Date getBirthDay() {
+		return birthDay;
+	}
+
+	public void setBirthDay(Date birthDay) {
+		this.birthDay = birthDay;
+	}
+
+	public boolean isMale() {
+		return male;
+	}
+
+	public void setMale(boolean male) {
+		this.male = male;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getFamilyContact() {
+		return familyContact;
+	}
+
+	public void setFamilyContact(String familyContact) {
+		this.familyContact = familyContact;
+	}
+
+	public Date getFirstJoin() {
+		return firstJoin;
+	}
+
+	public void setFirstJoin(Date firstJoin) {
+		this.firstJoin = firstJoin;
 	}
 
 }
