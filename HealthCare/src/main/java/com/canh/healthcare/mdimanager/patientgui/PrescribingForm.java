04dee9e8a69f024ceb1d714511f10a23ee2fe533
nguyenhuuca <nguyenhuuca@gmail.com>
2017-07-15 22:36:09 +0700

    adding PresceibingForm

--- HealthCare/src/main/java/com/canh/healthcare/mdimanager/patientgui/PrescribingForm.java
+++ HealthCare/src/main/java/com/canh/healthcare/mdimanager/patientgui/PrescribingForm.java
@@ -1,5 +1,6 @@
 package com.canh.healthcare.mdimanager.patientgui;
 
+import java.awt.BorderLayout;
 import java.awt.ComponentOrientation;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
@@ -8,13 +9,17 @@ import java.awt.Insets;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 public class PrescribingForm extends JInternalFrame {
-	
+
 	private JLabel lblId = new JLabel("Id bệnh nhân");
 	private JLabel lblName = new JLabel("Tên bệnh nhân");
 	private JTextField txtId = new JTextField("test", 10);
@@ -23,117 +28,214 @@ public class PrescribingForm extends JInternalFrame {
 	private JTextField txtBirthDate = new JTextField(10);
 	private JLabel lblGender = new JLabel("Giới tính");
 	private JTextField txtGender = new JTextField(10);
-	
-	private JLabel lblExamination= new JLabel("Ngày khám");
+
+	private JLabel lblExamination = new JLabel("Ngày khám");
 	private JTextField txtExamination = new JTextField(10);
-	private JLabel lblReExamination= new JLabel("Ngày khám");
+	private JLabel lblReExamination = new JLabel("Ngày khám");
 	private JTextField txtReExamination = new JTextField(10);
-	private JLabel lblPriceExamination= new JLabel("Tiền khám");
-	private JTextField txtPriveExamination = new JTextField(10);
-	private JLabel lblhourExamination= new JLabel("Số giờ khám");
+	private JLabel lblPriceExamination = new JLabel("Tiền khám");
+	private JTextField txtPriceExamination = new JTextField(10);
+	private JLabel lblHourExamination = new JLabel("Số giờ khám");
 	private JTextField txtHourExamination = new JTextField(5);
-	private JLabel lblTotalCost = new JLabel("Số giờ khám");
-	private JTextField txtTotalCost = new JTextField(10); 
-	
+	private JLabel lblTotalCost = new JLabel("Tổng tiền khám");
+	private JTextField txtTotalCost = new JTextField(10);
+	private JButton btnNewPrescribing = new JButton("Tạo mới");
+	private JButton btnUpdatePrescribing = new JButton("Cập nhật");
+
 	public PrescribingForm() {
 		super();
 		createPrescribingInput();
-		setSize(1000, 400);
+		createPrescribingArea();
+		createPrescribingArea();
+		createPrescribingList();
+		setSize(1000, 700);
+		// setPreferredSize(new Dimension(1000, 400));
 		setIconifiable(true);
 		setClosable(true);
 		this.setTitle("Bảng kê thuốc và chi phí bệnh nhân");
 		this.setVisible(true);
 		pack();
 	}
-	
-	public void createPrescribingInput(){
+
+	public void createPrescribingInput() {
+		// DefaultFormBuilder builder = new DefaultFormBuilder(new
+		// FormLayout(""));
 		JPanel prescribingPanel = new JPanel(new GridBagLayout());
-		prescribingPanel.setPreferredSize(new Dimension(950, 150));
+		prescribingPanel.setPreferredSize(new Dimension(800, 150));
 		GridBagConstraints constraints = new GridBagConstraints();
 		prescribingPanel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-		
 		constraints.anchor = GridBagConstraints.WEST;
-		//constraints.gridwidth = 0;
-		//constraints.weightx = 0;
-		//constraints.weighty = 0;
+		constraints.fill = GridBagConstraints.VERTICAL;
+		constraints.weightx = 0;
+		constraints.weighty = 0;
+
+		// row 1
 		constraints.gridx = 0;
 		constraints.gridy = 0;
-		prescribingPanel.add(lblName,constraints);
-		//constraints.anchor = GridBagConstraints.NORTH;
-		//constraints.gridwidth = 5;
-		//constraints.weightx = 1;
-		//constraints.weighty = 1;
-		
+		prescribingPanel.add(lblName, constraints);
+
 		constraints.gridx = 1;
-		constraints.gridy = 0;
 		txtName.setMinimumSize(txtName.getPreferredSize());
-		prescribingPanel.add(txtName,constraints);
-		
-		prescribingPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Kê Toa"));
-		
-		add(prescribingPanel);
-		
-		
+		prescribingPanel.add(txtName, constraints);
+
+		constraints.gridx = 2;
+		prescribingPanel.add(lblBirthDay, constraints);
+
+		constraints.gridx = 3;
+		txtBirthDate.setMinimumSize(txtBirthDate.getPreferredSize());
+		prescribingPanel.add(txtBirthDate, constraints);
+
+		constraints.gridx = 4;
+		prescribingPanel.add(lblGender, constraints);
+
+		constraints.gridx = 5;
+		txtGender.setMinimumSize(txtGender.getPreferredSize());
+		prescribingPanel.add(txtGender, constraints);
+
+		// row 2
+		constraints.gridx = 0;
+		constraints.gridy = 1;
+		prescribingPanel.add(lblId, constraints);
+
+		constraints.gridx = 1;
+		txtId.setMinimumSize(txtId.getPreferredSize());
+		prescribingPanel.add(txtId, constraints);
+
+		constraints.gridx = 2;
+		prescribingPanel.add(lblExamination, constraints);
+
+		constraints.gridx = 3;
+		txtExamination.setMinimumSize(txtExamination.getPreferredSize());
+		prescribingPanel.add(txtExamination, constraints);
+
+		constraints.gridx = 4;
+		prescribingPanel.add(lblReExamination, constraints);
+
+		constraints.gridx = 5;
+		txtReExamination.setMinimumSize(txtReExamination.getPreferredSize());
+		prescribingPanel.add(txtReExamination, constraints);
+
+		// row 3
+		constraints.gridx = 0;
+		constraints.gridy = 2;
+		prescribingPanel.add(lblPriceExamination, constraints);
+
+		constraints.gridx = 1;
+		txtPriceExamination.setMinimumSize(txtPriceExamination.getPreferredSize());
+		prescribingPanel.add(txtPriceExamination, constraints);
+
+		constraints.gridx = 2;
+		prescribingPanel.add(lblHourExamination, constraints);
+
+		constraints.gridx = 3;
+		txtHourExamination.setMinimumSize(txtHourExamination.getPreferredSize());
+		prescribingPanel.add(txtHourExamination, constraints);
+
+		constraints.gridx = 4;
+		prescribingPanel.add(lblTotalCost, constraints);
+
+		constraints.gridx = 5;
+		txtTotalCost.setMinimumSize(txtTotalCost.getPreferredSize());
+		prescribingPanel.add(txtTotalCost, constraints);
+
+		constraints.gridx = 2;
+		constraints.gridy = 3;
+		constraints.gridwidth = 6;
+		// constraints.insets = new Insets(20,20,20,20);
+		// constraints.anchor = GridBagConstraints.CENTER;
+		btnNewPrescribing.setActionCommand("NewPrescribing");
+		prescribingPanel.add(btnNewPrescribing, constraints);
+		constraints.gridx = 3;
+		btnUpdatePrescribing.setActionCommand("UpdatePrescribing");
+		prescribingPanel.add(btnUpdatePrescribing, constraints);
+		prescribingPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Tổng hợp"));
+
+		add(prescribingPanel, BorderLayout.NORTH);
+
 	}
-	
-	final static boolean shouldFill = true;
-    final static boolean shouldWeightX = true;
-    final static boolean RIGHT_TO_LEFT = false;
-	public void createPrescribingInput1(){
-		JPanel pane = new JPanel(new GridBagLayout());
-		pane.setPreferredSize(new Dimension(950, 150));
-		JButton button;
-		pane.setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
-		if (shouldFill) {
-		                //natural height, maximum width
-		                c.fill = GridBagConstraints.HORIZONTAL;
-		}
 
-		button = new JButton("Button 1");
-		if (shouldWeightX) {
-		                   c.weightx = 0.5;
-		}
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridx = 0;
-		c.gridy = 0;
-		pane.add(button, c);
-
-		button = new JButton("Button 2");
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.weightx = 0.5;
-		c.gridx = 1;
-		c.gridy = 0;
-		pane.add(button, c);
-
-		button = new JButton("Button 3");
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.weightx = 0.5;
-		c.gridx = 2;
-		c.gridy = 0;
-		pane.add(button, c);
-
-		button = new JButton("Long-Named Button 4");
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.ipady = 40;      //make this component tall
-		c.weightx = 0.0;
-		c.gridwidth = 3;
-		c.gridx = 0;
-		c.gridy = 1;
-		pane.add(button, c);
-
-		button = new JButton("5");
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.ipady = 0;       //reset to default
-		c.weighty = 1.0;   //request any extra vertical space
-		c.anchor = GridBagConstraints.PAGE_END; //bottom of space
-		c.insets = new Insets(10,0,0,0);  //top padding
-		c.gridx = 1;       //aligned with button 2
-		c.gridwidth = 2;   //2 columns wide
-		c.gridy = 2;       //third row
-		pane.add(button, c);
+	public void createPrescribingArea() {
+		JPanel prescribingArea = new JPanel();
+		prescribingArea.setLayout(null);
+		prescribingArea.setPreferredSize(new Dimension(800, 70));
+		// GridBagConstraints constraints = new GridBagConstraints();
+		prescribingArea.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		// constraints.anchor = GridBagConstraints.WEST;
+		// constraints.fill = GridBagConstraints.VERTICAL;
+		JLabel lblNameMedical = new JLabel("Tên thuốc:");
+		JComboBox cbxMedical = new JComboBox();
+		JLabel lblQuantity = new JLabel("Số lượng");
+		JTextField txtQuantity = new JTextField(5);
+		JButton btnAdd = new JButton("Xác nhận");
+		cbxMedical.setPreferredSize(new Dimension(150, 25));
+		Insets insets = prescribingArea.getInsets();
+		Dimension size = lblNameMedical.getPreferredSize();
+		int marginLeft = 25 + insets.left;
+		int margintTop = 25 + insets.top;
+		int width = size.width;
+		int height = size.height;
+		lblNameMedical.setBounds(marginLeft, margintTop, width, height);
+		size = cbxMedical.getPreferredSize();
+		marginLeft += width;
+		width = size.width;
+		height = size.height;
+		cbxMedical.setBounds(marginLeft, 20 + insets.top, width, height);
 		
-		add(pane);
+		size = lblQuantity.getPreferredSize();
+		marginLeft += width;
+		width = size.width;
+		height = size.height;
+		lblQuantity.setBounds(marginLeft, margintTop, width, height);
+		
+		size = txtQuantity.getPreferredSize();
+		marginLeft += width;
+		width = size.width;
+		height = size.height;
+		txtQuantity.setBounds(marginLeft, 20 + insets.top, width, height);
+		
+		size = btnAdd.getPreferredSize();
+		marginLeft += width;
+		width = size.width;
+		height = size.height;
+		btnAdd.setBounds(marginLeft, 20 + insets.top, width, height);
+		
+		prescribingArea.add(lblNameMedical);
+		prescribingArea.add(cbxMedical);
+		prescribingArea.add(lblQuantity);
+		prescribingArea.add(txtQuantity);
+		prescribingArea.add(btnAdd);
+
+		prescribingArea.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Kê toa"));
+		add(prescribingArea, BorderLayout.CENTER);
 	}
 
+	public void createPrescribingList() {
+		JPanel prescribingConsume = new JPanel();
+		prescribingConsume.setLayout(null);
+		prescribingConsume.setPreferredSize(new Dimension(800, 180));
+		// GridBagConstraints constraints = new GridBagConstraints();
+		prescribingConsume.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		// constraints.anchor = GridBagConstraints.WEST;
+		// constraints.fill = GridBagConstraints.VERTICAL;
+		JTable table = new JTable();
+		
+		DefaultTableModel model = new DefaultTableModel();
+		Object columnNames[] = { "Id", "Tên thuốc", "Số lượng", "Thành tiền" };
+		model.setColumnIdentifiers(columnNames);
+		table.setModel(model);
+		//populateJtable(model);
+		JScrollPane scrollPane = new JScrollPane(table);
+		scrollPane.setPreferredSize(new Dimension(500, 150));
+		Insets insets = prescribingConsume.getInsets();
+		Dimension size = scrollPane.getPreferredSize();
+		int marginLeft = 25 + insets.left;
+		int margintTop = 25 + insets.top;
+		int width = size.width;
+		int height = size.height;
+		scrollPane.setBounds(marginLeft,margintTop,width,height);
+		
+		prescribingConsume.add(scrollPane);
+		prescribingConsume.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Chi tiết"));
+		add(prescribingConsume, BorderLayout.SOUTH);
+	}
 }
